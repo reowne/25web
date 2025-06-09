6a00aa1857c9cba4187352a05654af701ac3f384
reowne <102846824+reowne@users.noreply.github.com>
2025-06-10 03:40:35 +0900

    Delete crafting life directory

--- crafting life/credit/credit.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const savedColor = localStorage.getItem('pointColor');
-    if (savedColor) {
-      document.documentElement.style.setProperty('--point-color', savedColor);
-    }
-});
-
